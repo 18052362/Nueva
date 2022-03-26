de24001523bebf8afb8293943d0b1a07f2d1e0d8
18052362 <101784781+18052362@users.noreply.github.com>
2022-03-25 20:08:46 -0600

    Update CuentaLineas.java

--- CuentaLineas.java
+++ CuentaLineas.java
@@ -1,29 +1,7 @@
-import java.io.*;
-import java.util.*;
-import javax.swing.*;
+public class HolaMundo {
 
-public class CuentaLineas
-{
-    static int lineasT;
+	public static void main(String[] args) {		
+		System.out.println("Hola Mundo");
+	}
 
-    public static void main(String[] args) {
-        lineasT = 0;
-        File archivo = new File("C:\\Users\\Diana Torres\\Desktop\\prueba.txt");
-        cuentaLineas(archivo);
-    }
-    
-    public static void cuentaLineas(File archivo) {
-        try {
-                BufferedReader archivoL = new BufferedReader(new FileReader(archivo));
-                String lineaL;
-                while((lineaL = archivoL.readLine()) != null) {
-                    lineasT= lineasT + 1;
-                }
-                JOptionPane.showMessageDialog(null, "Lineas totales: " + lineasT);
-                archivoL.close();
-            
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
 }
